939f64d5470e4f6a723a5b71a80a4f555739ce53
Rhyn Ogg <65290988+Dissurender@users.noreply.github.com>
2024-12-13 19:40:21 -0600

    add tailwind

---
+++ src/components/hero.tsx
@@ -0,0 +1,10 @@
+function LandingHero() {
+  return (
+    <div className="hero">
+      <h1>Welcome to the site!</h1>
+      <p>This is a simple hero component.</p>
+    </div>
+  );
+}
+
+export default LandingHero;
